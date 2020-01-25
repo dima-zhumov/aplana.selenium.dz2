1fe930839b36f107ed753cf845fc3683aab32b57
Dmitriy <dima-zhumov@yandex.ru>
2020-01-26 00:03:09 +0300

    Корректировка dz2

--- dz2-selenium-sberbank/src/main/java/Autotest2.java
+++ dz2-selenium-sberbank/src/main/java/Autotest2.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Locatable;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
