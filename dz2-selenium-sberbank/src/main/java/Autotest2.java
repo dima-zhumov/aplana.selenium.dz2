4dbbd830863bd90e90ceee906b32e037e9b6ea20
Dmitriy <dima-zhumov@yandex.ru>
2020-01-25 23:59:54 +0300

    Корректировка dz2

--- dz2-selenium-sberbank/src/main/java/Autotest2.java
+++ dz2-selenium-sberbank/src/main/java/Autotest2.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Autotest2 {
@@ -16,7 +18,7 @@ public class Autotest2 {
 
     @BeforeClass
     public static void TestUp(){
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Дмитрий/IdeaProjects/testing/driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
@@ -33,28 +35,28 @@ public class Autotest2 {
         webDriver.get("http://www.sberbank.ru/ru/person");
 
         WebDriverWait wait = new WebDriverWait(webDriver, 30);
+        String title;
 
-        webDriver.findElement(By.xpath("//a[contains(@class,'hd-ft-region')][1]")).click();
+        webDriver.findElement(By.xpath("//header//a[contains(@class,'hd-ft-region')]")).click();
 
         WebElement element = webDriver.findElement(By.xpath("//h4"));
         wait.until(ExpectedConditions.visibilityOf(element));
-        String title = element.getText();
-        Assert.assertTrue(title.contains("Выбор региона"));
 
-        WebElement Area = webDriver.findElement(By.xpath("//input[(@type='search') and contains(@class,'kit')]"));
-        Area.sendKeys("Нижегородская область");
+        webDriver.findElement(By.xpath("//input[(@type='search') and contains(@class,'kit')]")).sendKeys("Нижегородская область");
         webDriver.findElement(By.xpath("//a[contains(@class,'city')]")).click();
 
-        element = webDriver.findElement(By.xpath("//a[contains(@aria-label, 'Текущий')][1]"));
+        element = webDriver.findElement(By.xpath("//header//a[contains(@aria-label, 'Текущий')]"));
         title = element.getText();
-        Assert.assertTrue(title.contains("Нижегородская область"));
+        Assert.assertEquals("Ошибка. Регион не совпадает.",title,"Нижегородская область");
 
-        element = webDriver.findElement(By.xpath("//a[contains(@class,'footer__social')][1]"));
+        element = webDriver.findElement(By.xpath("//span[contains(@class,'footer__social_fb')]"));
         (((Locatable)element).getCoordinates()).inViewPort();
-        Thread.sleep(5000);
 
-        element=webDriver.findElement(By.xpath("//span[contains(@class,'footer__social')]"));
-        Assert.assertTrue(element.isDisplayed());
+
+        List<WebElement> elementList= webDriver.findElements(By.xpath("//span[contains(@class,'footer__social')]"));
+        for (WebElement webElement:elementList){
+            Assert.assertTrue(webElement.isDisplayed());
+        }
 
     }
 }
